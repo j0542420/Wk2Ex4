0bc1028849c5a08e1ee9effdb2dcfb4cb3c17ba3
Jose Villa <j0542420@amarillocollege.com>
2025-01-25 21:47:35 -0600

    update

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace Wk2Ex4
     {
         static void Main(string[] args)
         {
+            //declarations
+            int a = '0';
+            int b = '0';
+
+            //title of the calculator will be shown
+            Console.WriteLine("Welcome to the Hypotenuse Calculator!");
+
+            //user inputs the length of side a
+            Console.WriteLine("Enter the length of the first side (a): ");
+            //collect the user input of side a
+            a = Convert.ToInt32(Console.ReadLine());
+
+            //user inputs the length of side b
+            Console.WriteLine("Enter the length of the first side (b): ");
+            //collect the user input of side b
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //calculate the hypotenuse
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            //output the result
+            Console.WriteLine($"The length of the hypotenuse is : {c}");
+
+            //the calculator will thank the user
+            Console.WriteLine($"Thank you for using the Hypothenuse Calculator!");
         }
     }
 }
